c57646dd2d2676f34e27bfd3b1e56227b57f5ae3
Mario Miranda <mario.luiz.miranda@gmail.com>
2026-03-06 12:01:11 -0500

    Implémenter la recherche d'un sensor par id

--- src/main/java/com/codetruck/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/codetruck/algasensors/device/management/api/controller/SensorController.java
@@ -6,9 +6,11 @@ import com.codetruck.algasensors.device.management.common.IdGenerator;
 import com.codetruck.algasensors.device.management.domain.model.Sensor;
 import com.codetruck.algasensors.device.management.domain.model.SensorId;
 import com.codetruck.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
@@ -16,6 +18,12 @@ import org.springframework.web.bind.annotation.*;
 public class SensorController {
 
     private final SensorRepository sensorRepository;
+
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convertToModel(sensor);
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -30,6 +38,10 @@ public class SensorController {
                 .enabled(Boolean.FALSE)
                 .build();
         sensor = sensorRepository.saveAndFlush(sensor);
+        return convertToModel(sensor);
+    }
+
+    private SensorOutput convertToModel(Sensor sensor) {
         return SensorOutput.builder()
                 .id(sensor.getId().getValue())
                 .name(sensor.getName())
