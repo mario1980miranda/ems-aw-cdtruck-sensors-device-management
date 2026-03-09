94b3ff477e1e47511f9e10d52b654f6a9ebc9c6b
Mario Miranda <mario.luiz.miranda@gmail.com>
2026-03-09 17:18:11 -0400

    ResponseExceptionHandler

--- src/main/java/com/codetruck/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/codetruck/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -5,8 +5,12 @@ import com.codetruck.algasensors.device.management.api.client.SensorMonitoringCl
 import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
@@ -16,6 +20,7 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
     public SensorMonitoringClientImpl(RestClient.Builder builder) {
         this.restClient = builder
                 .baseUrl("http://localhost:8082")
+                .requestFactory(generateClientHttpRequestFactory())
                 .defaultStatusHandler(HttpStatusCode::isError, ((request, response) -> {
                     throw new SensorMonitoringClientBadGatewayException();
                 }))
@@ -36,5 +41,15 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
                 .uri("/api/sensors/{sensorID}/monitoring/disable", sensorId)
                 .retrieve()
                 .toBodilessEntity();
+    }
+
+
+    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        factory.setReadTimeout(Duration.ofSeconds(5));
+        factory.setConnectTimeout(Duration.ofSeconds(3));
+
+        return factory;
     }
 }
